897e9cc757621a126ba7839273507f4908033739
leopoldo <shout512@gmail.com>
2017-04-24 16:48:26 +0200

    update for database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds115131.mlab.com:15131/mppm');
+mongoose.connect('mongodb://shout512:!Inter79@ds115131.mlab.com:15131/mppm');
 
 module.exports = {mongoose};
